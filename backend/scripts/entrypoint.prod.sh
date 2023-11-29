250806e1b64b83505a0c962d04af80cb3af798fc
Stanislav <akacukee@mail.ru>
2023-11-29 17:11:07 +0400

    test5

--- backend/scripts/entrypoint.prod.sh
+++ backend/scripts/entrypoint.prod.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# python manage.py migrate
-# python manage.py collectstatic
-# cp -r /app/collected_static/. /backend_static/static/
+python manage.py migrate
+python manage.py collectstatic
+cp -r /app/collected_static/. /backend_static/static/
 
-sudo docker compose -f docker-compose.production.yml exec backend python manage.py migrate
-sudo docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic
-sudo docker compose -f docker-compose.production.yml exec backend cp -r /app/collected_static/. /backend_static/static/
